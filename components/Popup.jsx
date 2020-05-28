88f3e1a03c56c2fa4dcac44db1b550b3364072f8
oleksandr-ivanov <oleksandr.ivanov@schibsted.com>
2020-05-28 09:40:17 +0200

    Hardcode content

--- components/Popup.jsx
+++ components/Popup.jsx
@@ -8,7 +8,45 @@ export default function Popup({ text, closePopup }) {
     <div className={classNamesOuter}>
       <div className={classNamesInner}>
         <h1>{text}</h1>
-        <p></p>
+        <p className="py-6">
+          <b>Diversity is not just about visuals</b> - what we need is people
+          with different perspectives who challenge each others’ biases as we
+          develop products and services. Overlooking diversity and inclusion has
+          many different risks, not least in AI development.{" "}
+        </p>
+        <p className="py-6">
+          According to the AI Now Institute{" "}
+          <b>
+            there is a diversity crisis in the AI sector across gender and race.
+          </b>{" "}
+          Recent studies found only 18%of authors at leading AI conferences are
+          women, and more than 80%of AI professors are men. This disparity is
+          extreme in the AI industry: women comprise only 15% of AI research
+          staff at Facebook and 10% at Google. There is no public data on trans
+          workers or other gender minorities. For black workers, the picture is
+          even worse. For example, only 2.5% of Google’s workforce is black,
+          while Facebook and Microsoft are each at 4%.
+        </p>
+        <p className="py-6">
+          In England, the London Annual Business Survey analysed data from 7,615
+          firms to conclude that{" "}
+          <b>
+            ‘culturally diverse leadership teams were more likely to develop new
+            products
+          </b>{" "}
+          than those with homogenous leadership’. Homogeneous ‘groupthink’ (the
+          practice of making decisions within a group of people that all conform
+          to a certain demographic norm, i.e. team members are all of one gender
+          or ethnicity) can lead to <b>poor quality decision-making.</b>
+        </p>
+        <p className="py-6">
+          In Spain, research involving more than 4,277 companies discovered that
+          companies with{" "}
+          <b>
+            more women were more likely to introduce radical new innovations
+          </b>{" "}
+          into the market over a two-year.
+        </p>
         <button className={classNamesButton} onClick={closePopup}>
           Actions taken, take me further!
         </button>
